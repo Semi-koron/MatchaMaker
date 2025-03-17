7172bd958e00bd9e7cb1f8c4f94c3196da64de7d
Semi-koron <6jimoto6@gmail.com>
2025-03-17 14:27:30 +0900

    feat room function

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/Semikoron/Brachio/backend/millstone"
+	"github.com/gorilla/mux"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +19,13 @@ func main() {
 	if port == "" {
 		port = "8080" // ローカル環境用デフォルトポート
 	}
-	http.HandleFunc("/", handler)
-	fmt.Println("Server running on port", port)
-	http.ListenAndServe(":"+port, nil)
+	
+	r := mux.NewRouter()
+
+	// ルーム作成用の API
+	r.HandleFunc("/room/{roomID}", millstone.HandleWebSocket)
+
+	serverAddr := ":8080"
+	fmt.Println("WebSocket server started on", serverAddr)
+	log.Fatal(http.ListenAndServe(serverAddr, r))
 }
---
+++ backend/millstone/room.go
@@ -0,0 +1,65 @@
+package millstone
+
+// ルーム接続の処理
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+// WebSocket のアップグレーダー
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// ルーム管理用のマップ
+var rooms = make(map[string]map[*websocket.Conn]bool)
+
+// クライアントの WebSocket 接続を処理
+func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("test")
+	vars := mux.Vars(r)
+	roomID := vars["roomID"]
+
+	// WebSocket にアップグレード
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("WebSocket Upgrade Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	// ルームがなければ作成
+	if rooms[roomID] == nil {
+		rooms[roomID] = make(map[*websocket.Conn]bool)
+	}
+	rooms[roomID][conn] = true
+	log.Printf("Client connected to room: %s", roomID)
+
+	// クライアントのメッセージを受け取るループ
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read Error:", err)
+			delete(rooms[roomID], conn)
+			break
+		}
+
+		// ルーム内の全クライアントにメッセージを送信
+		for client := range rooms[roomID] {
+			err := client.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				log.Println("Write Error:", err)
+				client.Close()
+				delete(rooms[roomID], client)
+			}
+		}
+	}
+}
+
