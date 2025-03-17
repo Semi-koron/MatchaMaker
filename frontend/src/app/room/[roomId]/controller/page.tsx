70a5ab6357bf5c983d81e15f27b776a7d86a2259
Semi-koron <6jimoto6@gmail.com>
2025-03-17 16:36:04 +0900

    fix build error

--- frontend/src/app/room/[roomId]/controller/page.tsx
+++ frontend/src/app/room/[roomId]/controller/page.tsx
@@ -3,12 +3,9 @@ import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 
 export default function QRCodePage() {
-  const [isDarkmode, setIsDarkmode] = useState<boolean>(false);
   const param = useParams();
 
   useEffect(() => {
-    const isDarkmode = window.matchMedia("(prefers-color-scheme: dark)");
-    setIsDarkmode(isDarkmode.matches);
     const roomId = param.roomId;
   }, []);
 
