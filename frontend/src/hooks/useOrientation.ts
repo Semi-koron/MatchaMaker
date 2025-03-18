ff615e1fb6d405aefe4279ad524e2bee9ec31d21
Semi-koron <6jimoto6@gmail.com>
2025-03-18 17:41:53 +0900

    feat request permission

--- frontend/src/hooks/useOrientation.ts
+++ frontend/src/hooks/useOrientation.ts
@@ -1,5 +1,21 @@
 import { useState, useEffect } from "react";
 
+const requestPermission = async () => {
+  // @ts-expect-error only safari property
+  if (typeof DeviceOrientationEvent.requestPermission === "function") {
+    try {
+      const permissionState =
+        // @ts-expect-error only safari property
+        await DeviceOrientationEvent.requestPermission();
+      if (permissionState !== "granted") {
+        console.warn("Device Orientation permission denied.");
+      }
+    } catch (error) {
+      console.error("Error requesting Device Orientation permission:", error);
+    }
+  }
+};
+
 const useOrientation = () => {
   const [orientation, setOrientation] = useState<{
     alpha: number;
@@ -15,7 +31,7 @@ const useOrientation = () => {
         gamma: event.gamma ?? 0, // 左右の傾き（ロール）
       });
     };
-
+    requestPermission();
     window.addEventListener("deviceorientation", handleOrientation);
 
     return () => {
